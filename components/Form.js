6c1fc9f21d9d20e47dfadd17737dae2732547769
Stephen Boynton <boyntonsc@gmail.com>
2018-11-26 16:24:01 -0500

    Work on meal page

--- components/Form.js
+++ components/Form.js
@@ -2,20 +2,13 @@ import t from 'tcomb-form-native';
 import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
 
-
 const Form = t.form.Form;
-
-const Meal = t.struct({
-  'Meal Name': t.String,
-  'Description': t.String,
-  Ingredient: t.String
-})
 
 export default class MealForm extends Component {
   render() {
     return (
       <View>
-        <Form type={Meal} />
+        <Form type={this.props.type} />
       </View>
     )
   }
--- screens/MealsScreen.js
+++ screens/MealsScreen.js
@@ -1,14 +1,13 @@
 import React from 'react';
+import t from 'tcomb-form-native';
 import {
   StyleSheet,
   Text,
-  TextInput,
+  TouchableHighlight,
   View,
 } from 'react-native';
-import { WebBrowser } from 'expo';
-
-import { MonoText } from '../components/StyledText';
-import MealForm from '../components/MealForm';
+import Form from '../components/Form';
+import MostUsedCard from '../components/MostUsedCard'
 
 export default class HomeScreen extends React.Component {
   constructor(props) {
@@ -18,15 +17,36 @@ export default class HomeScreen extends React.Component {
     }
   }
 
+  mealForm = t.struct({
+    'Meal Name': t.String,
+    'Description': t.String,
+    Ingredients: t.String,
+    Favorite: t.Boolean
+  })
+
   static navigationOptions = {
     header: null,
   };
 
   render() {
     return (
-      <View>
-        <Text style={styles.title}>Create New Meals Here</Text>
-        <MealForm />
+      <View style={styles.view}>
+        <Text style={styles.header}>Meals</Text>
+        <TouchableHighlight style={styles.button}>
+          <Text>Add New</Text>
+        </TouchableHighlight>
+        <TouchableHighlight style={styles.button}>
+          <Text>Browse</Text>
+        </TouchableHighlight>
+        <View style={styles.mostUsed}>
+          <Text style={styles.subHeader}>Most Used</Text>
+          <MostUsedCard style={styles.card}
+            name='Ham and Swiss'
+            description='A tasty treat of both ham AND swiss :D'
+            ingredients={['ham', 'swiss', 'love', 'heat']}
+            uses='5'
+          />
+        </View>
       </View>
     );
   }
@@ -35,9 +55,33 @@ export default class HomeScreen extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  title: {
+  view: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    flexDirection: 'column',
+    backgroundColor: '#74b9ff',
+    flex: 1
+  },
+  button: {
+    backgroundColor: 'white',
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingLeft: 50,
+    paddingRight: 50,
+    borderRadius: 20,
+  },
+  header: {
     paddingTop: 50,
-    textAlign: 'center',
-    fontSize: 30
+    fontSize: 30,
+    color: 'white'
+  },
+  subHeader: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  mostUsed: {
+    padding: 5,
+    backgroundColor: 'white'
   }
 });
